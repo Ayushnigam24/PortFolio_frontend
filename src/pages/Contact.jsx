5a898478a080e3667ab81fd5315864c72c0685a1
Ayush Nigam <ayushnigamandh24@gmail.com>
2026-03-06 08:34:44 -0800

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -15,7 +15,7 @@ export default function Contact() {
  const handleSubmit = async (e) => {
   e.preventDefault();
   try {
-    await fetch("http://localhost:5000/api/contact", {
+    await fetch("https://portfolio-backend-xmv2.onrender.com/api/contact", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(formData),
