effba5b05604bdd85083aed7bc4b3ae32860b8af
Ayush Nigam <ayushnigamandh24@gmail.com>
2026-03-06 08:35:57 -0800

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -16,7 +16,7 @@ export default function Login() {
     e.preventDefault();
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/auth/login",
+        "https://portfolio-backend-xmv2.onrender.com/api/auth/login",
         form
       );
 
@@ -64,4 +64,5 @@ export default function Login() {
       </form>
     </div>
   );
+
 }
